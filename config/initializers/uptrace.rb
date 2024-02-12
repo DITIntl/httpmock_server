f704ee0cc989c22c88fee70e4f0134f4cf5725ea
Acho Arnold <arnold@ndolestudio.com>
2024-02-12 23:46:08 +0200

    Setup uptrace and logging

---
+++ config/initializers/uptrace.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'uptrace'
+require 'opentelemetry-instrumentation-rails'
+require 'opentelemetry-instrumentation-active_record'
+
+# copy your project DSN here or use UPTRACE_DSN env var
+Uptrace.configure_opentelemetry(dsn: ENV.fetch('UPTRACE_DSN')) do |c|
+  # c is OpenTelemetry::SDK::Configurator
+  c.use_all
+  c.service_name = 'httpmock'
+  c.service_version = ENV.fetch('VERSION', 'dev')
+  c.resource = OpenTelemetry::SDK::Resources::Resource.create(
+    OpenTelemetry::SemanticConventions::Resource::DEPLOYMENT_ENVIRONMENT => ENV.fetch(
+      'RAILS_ENV', 'development'
+    )
+  )
+end
