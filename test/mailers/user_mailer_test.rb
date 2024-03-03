4de542bd5fe0b19fc1e0a536cc6436c3954aaded
Acho Arnold <arnold@ndolestudio.com>
2024-03-03 12:46:19 +0200

    Fix test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,6 +6,6 @@ class UserMailerTest < ActionMailer::TestCase
   test 'confirmation' do
     mail = UserMailer.confirmation User.new, 'confirmation_token'
     assert_equal I18n.t('mailers.user.confirmation.subject'), mail.subject
-    assert_equal 'support@httpmock.dev', mail.from
+    assert_equal ['support@httpmock.dev'], mail.from
   end
 end
