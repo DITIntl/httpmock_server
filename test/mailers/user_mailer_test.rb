6f6c02f50bdefcea2eb857f3924fb862d29f3cbb
Acho Arnold <arnold@ndolestudio.com>
2024-03-03 12:44:53 +0200

    Fix user mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,10 +4,8 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test 'confirmation' do
-    mail = UserMailer.confirmation
-    assert_equal 'Confirmation', mail.subject
-    assert_equal ['to@example.org'], mail.to
-    assert_equal ['from@example.com'], mail.from
-    assert_match 'Hi', mail.body.encoded
+    mail = UserMailer.confirmation User.new, 'confirmation_token'
+    assert_equal I18n.t('mailers.user.confirmation.subject'), mail.subject
+    assert_equal 'support@httpmock.dev', mail.from
   end
 end
